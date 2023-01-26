fe8764ff0f148963ec011470616c6d170b734773
Youssef Attai <youssefgalalnazem@gmail.com>
2023-01-26 15:33:55 +0200

    ..

--- movieServer.js
+++ movieServer.js
@@ -1,22 +1,26 @@
+
 import * as dotenv from 'dotenv'
-import cors from 'cors'
 dotenv.config()
+
 import express from 'express'
 import mongoose from 'mongoose'
-import { router as movieRouter } from './routers/movieRouter.js'
+import cors from 'cors'
+import {router as movieRouter} from './routers/movieRouter.js'
 
 const MONGODB_CONNECTION = process.env.MONGODB_CONNECTION;
+
 mongoose.set('strictQuery', false);
 mongoose.connect(MONGODB_CONNECTION);
 
 const db = mongoose.connection;
 
 db.on("error", (err) => {
-    console.error(err);
+  console.error(err);
 });
 db.once("open", () => {
-    console.log("Connected to database");
+  console.log("Connected to database");
 });
+
 
 const app = express()
 const port = 3000
@@ -24,7 +28,7 @@ const port = 3000
 app.use(express.json());
 app.use(cors());
 
-app.use('/movie', movieRouter)
+app.use("/movie", movieRouter);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
